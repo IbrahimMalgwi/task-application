6fe659c3040bd852386a34b723097e213ad64fa6
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2023-06-01 16:47:03 +0100

    Created header and Footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import  Header from "./components/Header";
+import Footer  from "./components/Footer";
+import "./App.css"
+
 const App = () => {
    return (
      <>
+       <Header />
        <h1>Hello World!!!</h1>
        <p>This is a paragraph</p>
+       <p>K</p>
+       <Footer />
+
      </>
    );
 }
