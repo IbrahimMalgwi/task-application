a6e48bd83362e2252226fb6b2a5b8f7f2b55273d
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2023-06-04 00:59:59 +0100

    practice react hook useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import  Header from "./components/Header";
-import Footer  from "./components/Footer";
+import { useState } from "react";
 import "./App.css"
 
-const App = () => {
-   return (
-     <>
-       <Header />
-       <h1>Hello World!!!</h1>
-       <p>This is a paragraph</p>
-       <p>K</p>
-       <Footer />
 
-     </>
+const App = () => {
+  const [count, setCount] = useState(0);
+  
+  function handleAdd(){
+    setCount(count + 1);
+  }
+
+  function handleSub(){
+    setCount(count -1);
+  }
+
+  function handleReset(){
+    setCount(0);
+  }
+
+
+   return (
+     <div className="App">
+      <div className="box">
+        <p>{count}</p>  
+        <button onClick={handleAdd} className="add">ADD</button>       
+        <button onClick={handleSub } className="sub">SUB</button>
+        <button onClick={handleReset } className="reset">RESET</button>
+      </div>
+     </div>
    );
 }
 
